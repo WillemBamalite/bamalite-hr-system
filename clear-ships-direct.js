2415322ef81a82d6e8dbd982662327ce86091367
Willem van der Bent <willem@ambruinsma.nl>
2025-08-06 13:30:38 +0200

    Remove all debug and test files

--- clear-ships-direct.js
+++
@@ -1,9 +0,0 @@
-// Direct alle schepen verwijderen
-if (typeof window !== 'undefined') {
-  localStorage.removeItem('shipDatabase');
-  window.dispatchEvent(new Event('localStorageUpdate'));
-  window.dispatchEvent(new Event('forceRefresh'));
-  console.log('✅ Alle schepen zijn verwijderd uit localStorage!');
-} else {
-  console.log('❌ Window is niet beschikbaar');
-} 
